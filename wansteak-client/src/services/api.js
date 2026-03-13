0509b5778c116111f9f1845ff883588a9b243e6b
maulanadzikri <maulanadzikri05@gmail.com>
2026-03-13 14:35:51 +0700

    feat(auth): add global axios interceptor to gracefully handle 401 unauthorized errors

--- wansteak-client/src/services/api.js
+++ wansteak-client/src/services/api.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import toast from 'react-hot-toast'
 
 const api = axios.create({
     baseURL: 'http://localhost:8080/api',
@@ -19,5 +20,32 @@ api.interceptors.request.use((config) => {
 }, (error) => {
     return Promise.reject(error);
 });
+
+api.interceptors.response.use(
+    (response) => {
+        return response; // If the response succesful, just return as usual 
+    },
+    (error) => {
+        if (error.response && error.response.status === 401) {
+            // 1. Remove expired token
+            localStorage.removeItem('admin_token');
+
+            // 2. Tell the use to relog
+            toast.error("Sesi Anda telah berakhir. Silakan login kembali.", {
+                id: 'unauthorized-toast',
+                duration: 4000,
+            });
+
+            // 3. Redirect to login page
+            if (window.location.pathname !== '/login') {
+                setTimeout(() => {
+                    window.location.href = '/login';
+                }, 1000);
+            }
+        }
+
+        return Promise.reject(error);   
+    }
+)
 
 export default api;
