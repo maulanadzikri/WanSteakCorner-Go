627a27c5e73424e4a277da54281ea394e8857f09
maulanadzikri <maulanadzikri05@gmail.com>
2026-04-06 15:49:17 +0700

    chore: update api base url to production render

--- wansteak-client/src/services/api.js
+++ wansteak-client/src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import toast from 'react-hot-toast'
 
 const api = axios.create({
-    baseURL: 'http://localhost:8080/api',
+    baseURL: 'https://wansteakcorner-api.onrender.com/api',
     headers: {
         'Content-Type': 'application/json'
     },
